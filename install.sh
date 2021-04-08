a2b4c0187e8a5ab3938f9a62d8d3bfe3f3dc46d5
roc <rocsalvador001@protonmail.com>
2021-04-08 17:16:54 +0200

    shellcheck

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-cd src
+#!/bin/bash
+
+cd src || exit
 make
 mv pcstats ../pcstatsDeb/usr/bin/
 cd ..
 dpkg --build pcstatsDeb/
 mv pcstatsDeb.deb pcstats.deb
 echo -n "Do you want to install pcstats.deb? [Y/n] "
-read c
+read -r c
 if [ "$c" = "Y" ] || [ "$c" = "y" ] || [ "$c" = "" ]; then
     sudo dpkg -i pcstats.deb
-    rm pcstats.deb
 fi
