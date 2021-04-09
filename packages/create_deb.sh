b4ee9f08dce1ea07737623f8df45ec010bdbdffb
roc <rocsalvador001@protonmail.com>
2021-04-09 16:25:07 +0200

    packages folder

--- packages/create_deb.sh
+++ packages/create_deb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd src || exit
+cd ../src || exit
 make
-mv pcstats ../pcstatsDeb/usr/bin/
-cd ..
-dpkg --build pcstatsDeb/
-mv pcstatsDeb.deb pcstats.deb
+mv pcstats ../packages/pcstats_deb/usr/bin/
+cd ../packages
+dpkg --build pcstats_deb/
+mv pcstats_deb.deb pcstats.deb
 echo -n "Do you want to install pcstats.deb? [Y/n] "
 read -r c
 if [ "$c" = "Y" ] || [ "$c" = "y" ] || [ "$c" = "" ]; then
