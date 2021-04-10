6649ef1ee27f9110dceddefe3a4d0803fd6b38b6
roc <rocsalvador001@protonmail.com>
2021-04-10 19:40:17 +0200

    rm pcstats_deb folder

--- packages/create_deb.sh
+++ packages/create_deb.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-cd ../src || exit
-make
-mv pcstats ../packages/pcstats_deb/usr/bin/
-cd ../packages
+mkdir -p pcstats_deb/DEBIAN
+mkdir -p pcstats_deb/usr/bin
+mkdir -p pcstats_deb/usr/share/applications
+
+cp control pcstats_deb/DEBIAN
+cp pcstats.desktop pcstats_deb/usr/share/applications
+make -C ../src
+cp ../src/pcstats pcstats_deb/usr/bin/
+
 dpkg --build pcstats_deb/
 mv pcstats_deb.deb pcstats.deb
+
 echo -n "Do you want to install pcstats.deb? [Y/n] "
 read -r c
 if [ "$c" = "Y" ] || [ "$c" = "y" ] || [ "$c" = "" ]; then
