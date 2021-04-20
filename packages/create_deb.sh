4472462be714edbed10353b3cefbeab936c5d6ca
roc <rocsalvador001@protonmail.com>
2021-04-20 13:50:29 +0200

    input version

--- packages/create_deb.sh
+++ packages/create_deb.sh
@@ -4,15 +4,28 @@ mkdir -p pcstats_deb/DEBIAN
 mkdir -p pcstats_deb/usr/bin
 mkdir -p pcstats_deb/usr/share/applications
 
+echo -n  "Version: "
+read version
+
 echo "Package: pcstats
-Version: 3.0
+Version: $version
 Section: custom
 Architecture: amd64
 Depends: ncurses-base
 Maintainer: rocsalvador
 Description: Monitor CPU and RAM stats
 Homepage: https://github.com/rocsalvador/pcstats" > pcstats_deb/DEBIAN/control
-cp pcstats.desktop pcstats_deb/usr/share/applications
+
+echo "[Desktop Entry]
+Name=pcstats
+Version=$version
+Type=Application
+Comment=CPU and RAM monitor
+Terminal=true
+Exec=/usr/bin/pcstats
+Icon=utilities-system-monitor
+Categories=System;"  > pcstats_deb/usr/share/applications
+
 make -C ../src
 cp ../src/pcstats pcstats_deb/usr/bin/
 
