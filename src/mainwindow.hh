7eee017e3b5db9fa9ccf5e72d00a91b16981987b
roc <rocsalvador001@protonmail.com>
2021-10-01 13:53:06 +0200

    revert changes till before procs window

--- src/mainwindow.hh
+++ src/mainwindow.hh
@@ -2,8 +2,6 @@
 #define MAIN_WINDOW_HH
 
 #include "pcstats.hh"
-#include "processes.hh"
-
 #include <cstdlib>
 #include <list>
 #include <ncurses.h>
@@ -13,39 +11,22 @@ class main_window {
 private:
     //Private attributes
     
-    int showedWin;
-
-    int nWins;
-
     WINDOW* cpu_usage_win;
     WINDOW* ram_usage_win;
     WINDOW* core_usage_win;
     WINDOW* core_temps_win;
     WINDOW* core_freq_win;
     WINDOW* refresh_rate_win;
-
-    WINDOW* procsWin;
-    WINDOW* procSearchWin;
     
-    unsigned int max_stdsrc_height, max_stdsrc_width;
+    int max_stdsrc_height, max_stdsrc_width;
 
-    unsigned int core_wins_width;
+    int core_wins_width;
     
-    list<unsigned int> cpu_usage_history, ram_usage_history;
+    list<int> cpu_usage_history, ram_usage_history;
     
     double refresh_rate;
     
     pcstats *stats;
-
-    processes *procs;
-
-    int procOffset;
-
-    bool foundProc;
-
-    int foundProcOffset;
-
-    string procName;
 
     
     //Private functions
@@ -65,16 +46,12 @@ private:
     void print_core_freq();
     
     void print_all_win();
-
-    void printProcWin();
     
     void refresh_all_win();
     
     void set_refresh_rate(string refresh_rate);
     
-    void resizeStatsWin();
-
-    void resizeProcsWin();
+    void resize();
     
 public:
     main_window(const double& refresh_rate);
